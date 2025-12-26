346c8ae4c329bfe5fb2745f96833ef44f92cb119
CodeJunkie31 <ST10089304@vcconnect.edu.za>
2025-12-26 14:50:45 +0200

    Infrastructure Update

--- src/SafeWalk.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
+++ src/SafeWalk.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
@@ -31,3 +31,4 @@ namespace SafeWalk.Infrastructure.DependencyInjection
     }
 }
 
+
--- src/SafeWalk.Infrastructure/Persistence/SqlConnectionFactory.cs
+++ src/SafeWalk.Infrastructure/Persistence/SqlConnectionFactory.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace SafeWalk.Infrastructure.Persistence
 {
-    internal class SqlConnectionFactory
+    // IMPORTANT: this class MUST implement ISqlConnectionFactory
+    public class SqlConnectionFactory : ISqlConnectionFactory
     {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SafeWalkDb")
+                ?? throw new InvalidOperationException("Connection string 'SafeWalkDb' not found.");
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
+
